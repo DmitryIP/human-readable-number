6b5c72c26a1eb3225fa2e7a9a07b62b71e3f02b2
DmitryIP <dmitryip@tut.by>
2020-10-07 00:56:03 +0300

    feat: add new function from 0 to 999 999

--- src/index.js
+++ src/index.js
@@ -1,67 +1,71 @@
-module.exports = function toReadable(number) {
-    const map = {
-        0: "zero",
-        1: "one",
-        2: "two",
-        3: "three",
-        4: "four",
-        5: "five",
-        6: "six",
-        7: "seven",
-        8: "eight",
-        9: "nine",
-        10: "ten",
-        11: "eleven",
-        12: "twelve",
-        13: "thirteen",
-        14: "fourteen",
-        15: "fifteen",
-        16: "sixteen",
-        17: "seventeen",
-        18: "eighteen",
-        19: "nineteen",
-        20: "twenty",
-        30: "thirty",
-        40: "forty",
-        50: "fifty",
-        60: "sixty",
-        70: "seventy",
-        80: "eighty",
-        90: "ninety",
-    }
-    let string = '';
-    if (number === 0) return "zero";
+module.exports =
+    function toReadable(number) {
+        if (number > 999999) {
+            console.log('number should be less then 999,999');
+            return
+        };
 
-    function hundreds(number) {
-        let hund = Math.floor(number / 100);
-        switch (hund) {
-            case 0:
-                return dozens(number);
-                break;
-            default:
-                string = string + map[hund] + ' hundred ';
-                return dozens(number % 100);
+        const map = {
+            0: "zero",
+            1: "one",
+            2: "two",
+            3: "three",
+            4: "four",
+            5: "five",
+            6: "six",
+            7: "seven",
+            8: "eight",
+            9: "nine",
+            10: "ten",
+            11: "eleven",
+            12: "twelve",
+            13: "thirteen",
+            14: "fourteen",
+            15: "fifteen",
+            16: "sixteen",
+            17: "seventeen",
+            18: "eighteen",
+            19: "nineteen",
+            20: "twenty",
+            30: "thirty",
+            40: "forty",
+            50: "fifty",
+            60: "sixty",
+            70: "seventy",
+            80: "eighty",
+            90: "ninety",
         }
-    }
 
-    function dozens(num) {
-        let dec = Math.floor(num / 10);
-        let uni = num % 10;
-        if (num < 20 && num != 0) {
-            return string += map[num];
-        }
-        if (num >= 20) {
-            let dec = Math.floor(num / 10);
-            let uni = num % 10;
-            if (uni != 0) {
-                string += map[dec * 10] + ' ' + map[uni];
+        const ones = number % 10;
+        const tens = number % 100;
+        const hundreds = ((number - tens) / 100) % 10;
+        const thousands = Math.floor(number / 1000) % 1000;
+
+        if (number === 0) return 'zero';
+
+        function getTensAndOnes(tens) {
+            if (tens === 0) return '';
+            if (tens > 0 && tens < 20) return map[tens] + ' ';
+            if (!ones) {
+                return map[tens] + ' ';
             } else {
-                string += map[dec * 10]
+                return `${map[tens - ones]} ${map[ones]} `;
             }
-
         }
-        return string;
-    }
-    return hundreds(number).trim();
 
-}
+        function getHundreds(hundreds) {
+            return hundreds ? getTensAndOnes(hundreds) + 'hundred ' : '';
+        }
+
+        function getThousands(thousands) {
+            return thousands ? toReadable(thousands) + ' thousands ' : '';
+        }
+
+        return (getThousands(thousands) + getHundreds(hundreds) + getTensAndOnes(tens)).trim();
+
+    }
+
+
+// let r = toReadable(9);
+// console.log(r);
+
